c70c086a2a50db3aaac21712415f5ba71414d2f9
Rasmita9428 <rasmitaprajapati95@gmail.com>
2021-02-18 01:39:30 +0530

    getcontactdata

---
+++ app/src/main/java/com/example/contacts_pacewisdomsolutions/ContactList.java
@@ -0,0 +1,23 @@
+package com.example.contacts_pacewisdomsolutions;
+
+public class ContactList {
+    String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPhoneNo() {
+        return PhoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        PhoneNo = phoneNo;
+    }
+
+    String PhoneNo;
+}
--- app/src/main/java/com/example/contacts_pacewisdomsolutions/MainActivity.java
+++ app/src/main/java/com/example/contacts_pacewisdomsolutions/MainActivity.java
@@ -1,14 +1,174 @@
 package com.example.contacts_pacewisdomsolutions;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<ContactList> contacts;
 
+      public static final int REQUEST_READ_CONTACTS = 79;
+    ListView list;
+    ArrayList mobileArray;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
+                == PackageManager.PERMISSION_GRANTED) {
+            mobileArray = getAllContacts();
+
+        } else {
+            requestPermission();
+        }
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+        ContactlistAdapter adapter = new ContactlistAdapter(mobileArray);
+        rvContacts.setAdapter(adapter);
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+
+//        list = findViewById(R.id.list);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1, android.R.id.text1, mobileArray);
+//        list.setAdapter(adapter);
+    }
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+            // show UI part if you want here to show some rationale !!!
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS);
+        }
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mobileArray = getAllContacts();
+                } else {
+                    // permission denied,Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+    private ArrayList getAllContacts() {
+        ArrayList<String> nameList = new ArrayList<>();
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        if ((cur!= null ? cur.getCount() : 0) > 0) {
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+                String mobileno = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                nameList.add(name+mobileno);
+                if (cur.getInt(cur.getColumnIndex( ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    }
+                    pCur.close();
+                }
+            }
+        }
+        if (cur!= null) {
+            cur.close();
+        } return nameList;
+    }
+    public class ContactlistAdapter extends RecyclerView.Adapter<ContactlistAdapter.ViewHolder>{
+        ArrayList<String> mContacts=new ArrayList<>();
+
+        public ContactlistAdapter(ArrayList<String> contacts) {
+            this.mContacts=contacts;
+            Toast.makeText(MainActivity.this, mContacts.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        @NonNull
+        @Override
+        public ContactlistAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            // Inflate the custom layout
+            View contactView = inflater.inflate(R.layout.contact_items, parent, false);
+
+            // Return a new holder instance
+            ViewHolder viewHolder = new ViewHolder(contactView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ContactlistAdapter.ViewHolder holder, int position) {
+
+            // Set item views based on your views and data model
+            holder.tv_name.setText(mContacts.get(position));
+            holder.tv_phoneno.setText("999999");
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mContacts.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView tv_name,tv_phoneno;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+                tv_phoneno = (TextView) itemView.findViewById(R.id.tv_phoneno);
+
+            }
+        }
     }
 }
